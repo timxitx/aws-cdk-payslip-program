f6016213fde0162d44532799a6c97b8031d4bc34
Tianxu Xi <xitianxu36@gmail.com>
2021-08-17 13:37:30 +1000

    inital

--- bin/aws-cdk-payslip-program.ts
+++ bin/aws-cdk-payslip-program.ts
@@ -5,17 +5,10 @@ import { AwsCdkPayslipProgramStack } from '../lib/aws-cdk-payslip-program-stack'
 
 const app = new cdk.App();
 new AwsCdkPayslipProgramStack(app, 'AwsCdkPayslipProgramStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+    env: {
+      account: '837684165413',
+      region: 'us-east-2',
+    }
 });
+
+app.synth();
--- lib/aws-cdk-payslip-program-stack.ts
+++ lib/aws-cdk-payslip-program-stack.ts
@@ -1,9 +1,33 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
 
 export class AwsCdkPayslipProgramStack extends cdk.Stack {
+
+  public readonly urlOutput: cdk.CfnOutput;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // Required
+      cpu: 256, // Default is 256
+      desiredCount: 1, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 512, // Default is 512
+      publicLoadBalancer: false // Default is false
+    });
   }
 }
